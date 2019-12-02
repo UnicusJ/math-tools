5367a8d3220480793dd1537d8d1fca7c622a8ab5
Luke Polson <47739849+Luke-Polson@users.noreply.github.com>
2019-12-02 20:46:54 +1300

    added power set

---
+++ algorithms/power_set.py
@@ -0,0 +1,57 @@
+from binary_converter import *
+
+
+def power_set(s):
+    """
+    given a set (list) as input, generates the power set (set containing all subsets of original set)
+    function removes any duplicates from the original set
+
+    function uses binary representation to generate possible subsets. for example, for 2 elements, we count to 2^2
+    in binary:
+    00
+    01
+    10
+    11
+    which gives us our four permutations. no elements, element 2 only, element 1 only, and both elements
+    this generalises to sets of higher cardinality.
+
+    >>> power_set([1,2,3])
+    [[], [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]]
+
+    >>> power_set([])
+    [[]]
+
+    >>> power_set(['a'])
+    [[], ['a']]
+
+    """
+
+    s_copy = list(set(s))
+
+    # find maximum length of binary strings needed to generate all subset permutations
+    binary_size = 1
+    while len(s_copy) > binary_size:
+        binary_size = binary_size * 2
+
+    power_set_list = []
+
+    # power set has cardinality 2^(original set size)
+    for i in range(2**len(s_copy)):
+        subset = []
+        binary_num = binary_convert(i)
+        binary_string = '0'*(len(s_copy)-len(binary_num)) + binary_num
+
+        count = 0
+
+        for val in binary_string[::-1]:
+            if val == '1':
+                subset.append(s[count])
+            count += 1
+        power_set_list.append(subset)
+    power_set_list = sorted(power_set_list, key = lambda x: len(x))
+    return power_set_list
+
+
+
+
+
